727ab9480e7039a5141501cf7b8363ca0b51f66c
Giuseppe Mele <giuseppemele9410@gmail.com>
2025-12-09 16:00:32 +0100

    feat : esercizio 03 con funzione tradizionale e arrow functions

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -4,10 +4,42 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 
 // Dichiara la funzione qui.
+function arrayIni (array, letter) {
+
+    const newArray = []; 
+
+    for(let i = 0; i < array.length ; ++i) {
+
+        const word = array[i]; 
+
+        if(word[0] === letter) {
+            newArray.push(word); //aggiungo la parola intera
+        }
+    }
+
+    return newArray;
+}
+
+
+// arrow functions 
+// const arrayIni = (array,letter) => {
+//     const newArray = []; 
+
+//     for(let i = 0; i < array.length ; ++i) {
+
+//         const word = array[i]; 
+
+//         if(word[0] === letter) {
+//             newArray.push(word); //aggiungo la parola intera
+//         }
+//     }
+
+//     return newArray;
+// }
 
 
 // Invoca la funzione qui e stampa il risultato in console
-
+console.log(arrayIni(names,"A")); 
 
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
