2c67050efe844b6f14180f6343bada8cbf5db7cb
Giuseppe Mele <giuseppemele9410@gmail.com>
2025-12-09 15:43:01 +0100

    feat : esercizio 02 con funzione tradizionale e arrow functions

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -4,10 +4,35 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 
 // Dichiara la funzione qui.
+function getInitialsArray (wordsArray) {
 
+    const initials = []; //array delle iniziali
+
+    //ciclo sull'array 
+    for(let i = 0; i < wordsArray.length ; ++i) {
+      const word = wordsArray[i]; //prendo la parola all'indice [i] e la salvo in una costante 
+      const initial = word[0]; // prendp la prima lettera della parola (in posizione 0) e la salvo in una costante 
+      initials.push(initial); //aggiungo l'iniziale nel nuovo array 
+    }
+    return initials ; 
+
+}
+
+//arrow functions 
+// const getInitialsArray = (wordsArray) => {
+//     const initials = []; //array delle iniziali
+
+//     //ciclo sull'array 
+//     for(let i = 0; i < wordsArray.length ; ++i) {
+//       const word = wordsArray[i]; //prendo la parola all'indice [i] e la salvo in una costante 
+//       const initial = word[0]; // prendp la prima lettera della parola (in posizione 0) e la salvo in una costante 
+//       initials.push(initial); //aggiungo l'iniziale nel nuovo array 
+//     }
+//     return initials ; 
+// }
 
 // Invoca la funzione qui e stampa il risultato in console
-
+console.log(getInitialsArray(names)); 
 
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
