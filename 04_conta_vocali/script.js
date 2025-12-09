44fa8ca41571ddd0e342f6788eb12272400cdef2
Giuseppe Mele <giuseppemele9410@gmail.com>
2025-12-09 16:18:57 +0100

    feat : esercizio 04 con funziona tradizionale e arrow functions

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -5,9 +5,44 @@ const word = 'javascript';
 
 // Dichiara la funzione qui.
 
+function vowelsCount (str) {
+
+    let counter = 0; 
+    const vowelsFound = []; //mi serve per salvare le vocali in un nuovo array 
+
+    for ( let i = 0; i < str.length ; ++i) { //ciclo sulla stringa 
+        if (str[i] === 'a' || str[i] === 'e' || str[i] === 'i' || str[i] === 'o' || str[i] === 'u') {
+           
+           // se all'indice in cui sto ciclando trovo una vocale , incremento il count e salvo in un nuovo array 
+            ++counter ; 
+            vowelsFound.push(str[i]); 
+        }
+    }
+    
+    return `${counter} (${vowelsFound.join(',')}) `; 
+
+}
+
+
+//arrow functions 
+// const vowelsCount = (str) => {
+//       let counter = 0; 
+//     const vowelsFound = []; //mi serve per salvare le vocali in un nuovo array 
+
+//     for ( let i = 0; i < str.length ; ++i) { //ciclo sulla stringa 
+//         if (str[i] === 'a' || str[i] === 'e' || str[i] === 'i' || str[i] === 'o' || str[i] === 'u') {
+           
+//            // se all'indice in cui sto ciclando trovo una vocale , incremento il count e salvo in un nuovo array 
+//             ++counter ; 
+//             vowelsFound.push(str[i]); 
+//         }
+//     }
+    
+//     return `${counter} (${vowelsFound.join(',')}) `; 
+// }
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(vowelsCount(word)); 
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
